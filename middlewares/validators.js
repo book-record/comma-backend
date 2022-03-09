ba665cef4ef8ebd06ead2a2a989dd3bf737a64c7
yohrran <johnyohan93@gmail.com>
2022-03-10 00:27:16 +0900

    fix: make middleware

--- middlewares/validators.js
+++ middlewares/validators.js
@@ -5,13 +5,16 @@ const {
   checkBookId,
   checkTypeOfBoolean,
   checkReviewId,
+  checkmongooseId,
 } = require('../utils/validator');
 
 exports.validateCreateAudio = async (req, res, next) => {
   const { id, userId } = req.params;
 
-  checkUserId(userId);
-  checkBookId(id);
+  if (await checkmongooseId(id)) return next('유효하지 않은 id 입니다');
+  if (await checkBookId(id)) return next('유효하지 않은 bookId 입니다');
+  if (await checkmongooseId(userId)) return next('유효하지 않은 id 입니다');
+  if (await checkUserId(userId)) return next('유효하지 않은 userId 입니다');
 
   return next();
 };
@@ -19,7 +22,8 @@ exports.validateCreateAudio = async (req, res, next) => {
 exports.validateBookId = async (req, res, next) => {
   const { id } = req.params;
 
-  checkBookId(id);
+  if (await checkmongooseId(id)) return next('유효하지 않은 id 입니다');
+  if (await checkBookId(id)) return next('유효하지 않은 bookId 입니다');
 
   return next();
 };
@@ -27,10 +31,10 @@ exports.validateBookId = async (req, res, next) => {
 exports.validateBook = async (req, res, next) => {
   const { title, authors, thumbnail, contents } = req.body;
 
-  checkTypeOfString(title);
-  checkTypeOfString(authors[0]);
-  checkTypeOfString(thumbnail);
-  checkTypeOfString(contents);
+  if (checkTypeOfString(title)) return next('type은 string이 아닙니다');
+  if (checkTypeOfString(authors[0])) return next('type은 string이 아닙니다');
+  if (checkTypeOfString(thumbnail)) return next('type은 string이 아닙니다');
+  if (checkTypeOfString(contents)) return next('type은 string이 아닙니다');
 
   return next();
 };
@@ -38,7 +42,8 @@ exports.validateBook = async (req, res, next) => {
 exports.validateUserId = async (req, res, next) => {
   const { id } = req.params;
 
-  checkUserId(id);
+  if (await checkmongooseId(id)) return next('유효하지 않은 id 입니다');
+  if (await checkUserId(id)) return next('유효하지 않은 userId 입니다');
 
   return next();
 };
@@ -46,7 +51,8 @@ exports.validateUserId = async (req, res, next) => {
 exports.validateReportId = async (req, res, next) => {
   const { id } = req.params;
 
-  checkReportId(id);
+  if (await checkmongooseId(id)) return next('유효하지 않은 id 입니다');
+  if (await checkReportId(id)) return next('유효하지 않은 reportId 입니다');
 
   return next();
 };
@@ -55,13 +61,15 @@ exports.validateReport = async (req, res, next) => {
   const { id, bookTitle, imageUrl, title, text, startDate, finishDate } =
     req.body;
 
-  checkUserId(id);
-  checkTypeOfString(bookTitle);
-  checkTypeOfString(imageUrl);
-  checkTypeOfString(title);
-  checkTypeOfString(text);
-  checkTypeOfString(startDate);
-  checkTypeOfString(finishDate);
+  if (await checkmongooseId(id)) return next('유효하지 않은 id 입니다');
+  if (await checkUserId(id)) return next('유효하지 않은 userId 입니다');
+
+  if (checkTypeOfString(bookTitle)) return next('type은 string이 아닙니다');
+  if (checkTypeOfString(imageUrl)) return next('type은 string이 아닙니다');
+  if (checkTypeOfString(title)) return next('type은 string이 아닙니다');
+  if (checkTypeOfString(text)) return next('type은 string이 아닙니다');
+  if (checkTypeOfString(startDate)) return next('type은 string이 아닙니다');
+  if (checkTypeOfString(finishDate)) return next('type은 string이 아닙니다');
 
   return next();
 };
@@ -69,9 +77,12 @@ exports.validateReport = async (req, res, next) => {
 exports.validateReview = async (req, res, next) => {
   const { reviewerId, userId, good } = req.body;
 
-  checkReviewId(reviewerId);
-  checkUserId(userId);
-  checkTypeOfBoolean(good);
+  if (await checkmongooseId(reviewerId)) return next('유효하지 않은 id 입니다');
+  if (await checkReviewId(reviewerId))
+    return next('유효하지 않은 reviewerId 입니다');
+  if (await checkmongooseId(userId)) return next('유효하지 않은 id 입니다');
+  if (await checkUserId(userId)) return next('유효하지 않은 userId 입니다');
+  if (checkTypeOfBoolean(good)) return next('type이 boolean이 아닙니다');
 
   return next();
 };
--- utils/validator.js
+++ utils/validator.js
@@ -7,60 +7,51 @@ const User = require('../models/User');
 
 function checkTypeOfString(string) {
   if (!(typeof string === 'string')) {
-    throw new Error('글자가 아닙니다');
+    return true;
   }
 }
 
 function checkTypeOfBoolean(boolean) {
   if (!(typeof boolean === 'boolean')) {
-    throw new Error('boolean이 아닙니다');
+    return true;
+  }
+}
+
+async function checkmongooseId(id) {
+  if (!mongoose.isValidObjectId(id)) {
+    return true;
   }
 }
 
 async function checkUserId(id) {
   const user = await User.findById({ _id: id });
 
-  if (!mongoose.isValidObjectId(id)) {
-    throw new Error('유효하지 않은 user id입니다');
-  }
-
   if (!user) {
-    throw new Error('해당 userId를 찾을 수 없습니다');
+    return true;
   }
 }
 
 async function checkBookId(id) {
   const book = await Book.findById(id);
 
-  if (!mongoose.isValidObjectId(id)) {
-    throw new Error('유효하지 않은 book id입니다');
-  }
-
   if (!book) {
-    throw new Error('해당 bookId를 찾을 수 없습니다');
+    return true;
   }
 }
 
 async function checkReportId(id) {
   const report = await Report.findById(id);
 
-  if (!mongoose.isValidObjectId(id)) {
-    throw new Error('유요하하지 않은 report id입니다');
-  }
-
   if (!report) {
-    throw new Error('해당 reportId를 찾을 수 없습니다');
+    return true;
   }
 }
 
 async function checkReviewId(id) {
   const review = await Review.findById(id);
-  if (!mongoose.isValidObjectId(id)) {
-    throw new Error('유요하하지 않은 report id입니다');
-  }
 
   if (!review) {
-    throw new Error('해당 reportId를 찾을 수 없습니다');
+    return true;
   }
 }
 
@@ -71,4 +62,5 @@ module.exports = {
   checkReportId,
   checkTypeOfBoolean,
   checkReviewId,
+  checkmongooseId,
 };
